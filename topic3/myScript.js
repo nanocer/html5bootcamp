12ed552257841c1285b377ba4b732d9fd0f57dc2
nanocer <jicerrutti@gmail.com>
2016-04-15 15:49:31 -0300

    Post and Delete finished

--- topic3/myScript.js
+++ topic3/myScript.js
@@ -4,9 +4,33 @@ myApp.controller('MoviesCtrl', ['$scope', '$http', function ($scope, $http) {
 
     var mov=this;
     $http.get('http://localhost:3000/movies').success(function(respuesta){
-        console.log("res:", respuesta);
         mov.peliculas = respuesta;
     });
+
+    mov.fdata={};
+
+    mov.enviar = function(){
+        $http.post('http://localhost:3000/movies', mov.fdata)
+         .success(function(res){
+            console.log(res);
+            $http.get('http://localhost:3000/movies').success(function(respuesta){
+            mov.peliculas = respuesta;
+            });
+            
+        });
+    }
+
+    $scope.deleteMovie = function(index){
+        console.log($scope.mov.peliculas[index].id)
+        $http.delete('http://localhost:3000/movies/' + $scope.mov.peliculas[index].id)
+            .success(function(res){
+               console.log(res);
+                $http.get('http://localhost:3000/movies').success(function(respuesta){
+                mov.peliculas = respuesta; 
+            });
+
+        });
+    }
     
 
 }]);
