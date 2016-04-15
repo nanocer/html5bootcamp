10327c70af755b6b7cfa676c24651af1aad81870
nanocer <jicerrutti@gmail.com>
2016-04-15 14:26:20 -0300

    Get method

--- topic3/myScript.js
+++ topic3/myScript.js
@@ -1,38 +1,12 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MoviesCtrl', ['$scope', function ($scope) {
+myApp.controller('MoviesCtrl', ['$scope', '$http', function ($scope, $http) {
 
-
-    $scope.movie = {};
-
-    $scope.movie.details = [{
-        "title": "Star Wars",
-        "year": "2015",
-        "duration": "120",
-        "img": "https://lh3.googleusercontent.com/-H1KqX5HicIU/AAAAAAAAAAI/AAAAAAABOuo/ENj4lcl2ekE/s0-c-k-no-ns/photo.jpg",
-        
-    },{
-        "title": "Titanic",
-        "year": "1997",
-        "duration": "120",
-        "img": "http://www.actitudfem.com/media/files/styles/gallerie_carousel/public/images/2015/04/titanicportada_0.jpg?itok=Nak3aC0A",
-        
-    },{
-        "title": "Halloween",
-        "year": "1998",
-        "duration": "120",
-        "img": "http://screenrant.com/wp-content/uploads/Halloween.jpg",
-        
-    },{
-        "title": "Friday 13",
-        "year": "1990",
-        "duration": "120",
-        "img": "http://legionofleia.com/wp-content/uploads/2015/08/friday-the-13th.jpg",
-        
-    }];
-
-    $scope.deleteMovie = function (index) {
-        $scope.movie.details.splice(index, 1)
-    };
+    var mov=this;
+    $http.get('http://localhost:3000/movies').success(function(respuesta){
+        console.log("res:", respuesta);
+        mov.peliculas = respuesta;
+    });
+    
 
 }]);
